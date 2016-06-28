ef20a8f44ade0f0f26b544986a523669bc4faf49
Ellen Shin <ellenshin1001@gmail.com>
2016-06-27 20:28:31 -0700

    Initial Commit

---
+++ Social Cat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social Cat
+//
+//  Created by Ellen Shin on 6/27/16.
+//  Copyright © 2016 Ellen Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
